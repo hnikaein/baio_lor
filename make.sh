3baa2e2e4cde58d65d840070f0178a72caa49a0b
nikaein <nikaein@gmail.com>
2018-02-03 00:10:41 +0330

    Adding make.sh

---
+++ make.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+output_file_name=outputs/te26
+read_file_name=grch38.reads3.fastq
+
+rm $output_file_name
+rm c_long_read_aligner
+cmake --build cmake-build-debug/ --target c_long_read_aligner -- -j 2 && mv cmake-build-debug/c_long_read_aligner .
+chmod +x c_long_read_aligner
+./c_long_read_aligner --ref=seqs/grch38.fasta --reads=seqs/$read_file_name --log=5 --read-index=false > $output_file_name &
