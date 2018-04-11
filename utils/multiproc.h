cd49ee7a0dcc1f2bccf867627ef954c6893e34e5
nikaein <nikaein@gmail.com>
2018-04-11 16:37:10 +0430

    Run no thread for 1 thread, multiproc for vector

--- utils/multiproc.h
+++ utils/multiproc.h
@@ -1,8 +1,12 @@
+#include <vector>
+
 #ifndef C_LONG_READ_ALIGNER_MULTIPROC_H
 #define C_LONG_READ_ALIGNER_MULTIPROC_H
 
 void *consumer_thread(void *count);
 
 int multiproc(int thread_count, int (*start_routine)(const int), int end, int from = 0);
+
+int multiproc(int thread_count, int (*start_routine)(const int), std::vector<int> ids);
 
 #endif //C_LONG_READ_ALIGNER_MULTIPROC_H
